73c7da0491ccb8fb4ce8fea85d1bc0d028f5b018
anikeaty08 <aniketbxr1@gmail.com>
2025-12-15 22:31:53 +0530

    Auto-sync after agent response

--- realtime-server/index.js
+++ realtime-server/index.js
@@ -86,6 +86,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Realtime server listening on :${PORT}`);
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Realtime server listening on http://127.0.0.1:${PORT}`);
 });
